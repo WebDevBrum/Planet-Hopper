1a79b1e5af1078e11079bae33b1b654731b883f1
Scott Davies <scott@scottdavies.uk>
2023-04-20 15:50:43 +0100

    updated type interface

--- global.d.ts
+++ global.d.ts
@@ -1,26 +1,30 @@
+interface Details {
+  content: string;
+  source: string;
+}
+
+interface Images {
+  planet: string;
+  internal: string;
+  geology: string;
+}
+
 interface PlanetData {
   _id: string;
   name: string;
-  overview: {
-    content: string;
-    source: string;
-  };
-  structure: {
-    content: string;
-    source: string;
-  };
-  geology: {
-    content: string;
-    source: string;
-  };
+  overview: Details;
+  structure: Details;
+  geology: Details;
   rotation: string;
   revolution: string;
   radius: string;
   temperature: string;
-  images: {
-    planet: string;
-    internal: string;
-    geology: string;
-  };
+  images: Images;
   [key: string]: any;
 }
+
+// alternative here could be
+
+// interface PlanetData {
+//   [key: string]: string | object;
+// }
